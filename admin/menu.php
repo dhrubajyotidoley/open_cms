281d033410c8660371f745375b49666d63119222
Dhrubajyoti Doley <dhruba.doley@gmail.com>
2015-04-18 23:21:35 +0530

    Update menu.php

--- admin/menu.php
+++ admin/menu.php
@@ -7,6 +7,19 @@
                     <li>
                         <a href="form_builder.php"><i class="fa fa-fw fa-file-code"></i> Form Builder</a>
                     </li>
+<?php
+$data_p = mysql_query("SELECT * FROM menu");
+$k=1;
+
+while($info = mysql_fetch_array( $data_p ))
+{
+	$name = substr($info['name'], 0, -4);
+	$name=ucfirst($name);
+	echo '<li>
+                        <a href="'.$info['name'].'"><i class="fa fa-fw file-code"></i>'.$name.'</a>
+                    </li>';
+	}
+?>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
